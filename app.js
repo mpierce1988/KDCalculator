65b820ce350c496d8349f18a91e5fb45e0d4f239
mpierce1988 <mpierce1988@gmail.com>
2022-02-17 15:52:47 -0400

    Completed Functions

--- app.js
+++ app.js
@@ -3,3 +3,43 @@
     DATE: Feb 17, 2022
     PURPOSE: Kill Death Ratio Calculator
 */
+
+// Functions
+
+let myGame = '';
+let stats = [];
+let avgKDR = 0;
+let add = new String();
+
+function calculateKDR(kills, deaths) {
+  if (deaths <= 0) {
+    return kills;
+  }
+
+  return kills / deaths;
+}
+
+let calculateAvgKDR = function () {
+  let totalKDR = 0;
+  stats.forEach((element) => {
+    totalKDR += stats[element];
+  });
+
+  return totalKDR / stats.length;
+};
+
+let promptForNumber = (msg) => {
+  return parseInt(prompt(msg, 0));
+};
+
+let validateKDRInputs = (kills, deaths) => {
+  if (isNaN(kills) || isNaN(deaths)) {
+    return false;
+  }
+
+  return true;
+};
+
+let dw = (content) => {
+  document.writeln(content);
+};
